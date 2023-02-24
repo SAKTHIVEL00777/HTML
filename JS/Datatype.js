e772ef46a3a696b5643a82041e7f0c71cebcf85d
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-02-24 17:20:42 +0530

    datatype using for string functions

--- JS/Datatype.js
+++ JS/Datatype.js
@@ -2,8 +2,10 @@ var no=27;
 var boo=true;
 var nul=null;
 var undefine=undefined;
+var str='String';
 
 console.log(typeof(no));
 console.log(typeof(boo));
 console.log(typeof(nul));
 console.log(typeof(undefine));
+console.log(typeof(str));
